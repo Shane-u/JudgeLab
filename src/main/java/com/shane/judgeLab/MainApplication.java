347dc6159b017c216ddb1b3180c020757ecf568c
Shane <1554096735@qq.com>
2025-09-25 16:57:42 +0800

    add initial project configuration and setup files

---
+++ src/main/java/com/shane/judgeLab/MainApplication.java
@@ -0,0 +1,23 @@
+package com.shane.judgeLab;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Author: Shane
+ * @Date: 2025/09/25/16:34
+ * @Description:
+ */
+@SpringBootApplication
+@MapperScan("com.shane.judgeLab.mapper")
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
